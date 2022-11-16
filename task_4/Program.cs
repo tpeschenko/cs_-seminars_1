d1fc1846d04ebbf51659a229e2fd33f9fe61c58f
Taras <taras.peschenko.90@mail.ru>
2022-11-17 01:38:37 +0300

    Четвёртая задача

---
+++ task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N > 0), а на выходе показывает все чётные числа от 1 до N.
+
+int input_number = 30;
+int output_number = 1;
+
+while (output_number <= input_number)
+{
+    if (output_number % 2 == 0)
+    {
+        Console.Write(output_number);
+        Console.Write(" ");
+    }
+    output_number += 1;
+}
+
+
